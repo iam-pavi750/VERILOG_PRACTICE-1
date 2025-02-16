26079f7c0415973185726d2c2f1c6031c506504a
iam-pavi750 <110714667+iam-pavi750@users.noreply.github.com>
2025-02-16 20:08:03 +0530

    Update half_subractor.sv

--- DAY 1/half_subractor.sv
+++ DAY 1/half_subractor.sv
@@ -6,3 +6,33 @@ module half_subtractor(input a, b, output diff, borr);
   and a2(borr,abar,b);
   
 endmodule
+
+
+module half;
+  
+  reg a,b;
+  wire diff,borr;
+  
+  half_subtractor h1(.a(a),.b(b),.diff(diff),.borr(borr));
+  
+  
+  initial begin
+     a= 0 ;b = 0;
+    #1 a =0; b= 1;
+    #1 a= 1; b= 0;
+    #1 a = 1; b= 1;
+    
+    #1 $finish;
+  end
+  
+  initial begin
+    $dumpfile("half_subtractor.vcd");
+    $dumpvars(0,half);
+    
+  end
+  
+  always@(a or b)
+    $display(" a= %0b b= %0b diff = %0b borr =%0b",a,b,diff,borr);
+  
+endmodule
+
